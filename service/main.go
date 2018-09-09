da1002fabe5bae941b4ba9ac748a618398dac044
XiaoqiJiang <XiaoqiJiang@Admins-MBP-2.attlocal.net>
2018-09-08 20:04:08 -0700

    authentication

--- service/main.go
+++ service/main.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	elastic "gopkg.in/olivere/elastic.v3"
-	"fmt"
-	"net/http"
+	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
-	"strconv"
-	"github.com/pborman/uuid"
+	"net/http"
 	"reflect"
-)
+	"strconv"
+	"strings"
 
-const (
-      INDEX = "around"
-      TYPE = "post"
-      DISTANCE = "200km"
-      // Needs to update
-      PROJECT_ID = "neighbor-post"
-      //BT_INSTANCE = "around-post"
-      // Needs to update this URL if you deploy it to cloud.
-      ES_URL = "http://35.202.69.40:9200"
+	"cloud.google.com/go/storage"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/pborman/uuid"
+	elastic "gopkg.in/olivere/elastic.v3"
 )
 
 type Location struct {
@@ -29,65 +26,204 @@ type Location struct {
 
 type Post struct {
 	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
-	User     string `json:"user"`
-	Message  string  `json:"message"`
+	User     string   `json:"user"`
+	Message  string   `json:"message"`
 	Location Location `json:"location"`
+	Url      string   `json:"url"`
 }
+
+var mySigningKey = []byte("secret")
 
 func main() {
 	// Create a client
-      client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
-      if err != nil {
-             panic(err)
-             return
-      }
-
-      // Use the IndexExists service to check if a specified index exists.
-      exists, err := client.IndexExists(INDEX).Do()
-      if err != nil {
-             panic(err)
-      }
-      if !exists {
-             // Create a new index.
-             mapping := `{
-                    "mappings":{
-                           "post":{
-                                  "properties":{
-                                         "location":{
-                                                "type":"geo_point"
-                                         }
-                                  }
-                           }
-                    }
-             }
-             `
-             _, err := client.CreateIndex(INDEX).Body(mapping).Do()
-             if err != nil {
-                    // Handle error
-                    panic(err)
-             }
-      }
-	fmt.Println("started-service")
-	http.HandleFunc("/post", handlerPost)
-	http.HandleFunc("/search", handlerSearch)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-
-func handlerPost(w http.ResponseWriter, r *http.Request) {
-	// Parse from body of request to get a json object.
-	fmt.Println("Received one post request")
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
 		panic(err)
 		return
 	}
 
-	id := uuid.New()
-        // Save to ES.
-        saveToES(&p, id)
+	// Use the IndexExists service to check if a specified index exists.
+	exists, err := client.IndexExists(INDEX).Do()
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		// Create a new index.
+		mapping := `{
+								"mappings":{
+											 "post":{
+															"properties":{
+																		 "location":{
+																						"type":"geo_point"
+																		 }
+															}
+											 }
+								}
+				 }
+				 `
+		_, err := client.CreateIndex(INDEX).Body(mapping).Do()
+		if err != nil {
+			// Handle error
+			panic(err)
+		}
+	}
+	fmt.Println("started-service")
 
+	r := mux.NewRouter()
+
+	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
+	//http.HandleFunc("/post", handlerPost)
+	//http.HandleFunc("/search", handlerSearch)
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+	r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+	r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+const (
+	INDEX    = "around"
+	TYPE     = "post"
+	DISTANCE = "200km"
+	// Needs to update
+	//PROJECT_ID = "around-xxx"
+	//BT_INSTANCE = "around-post"
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://35.188.168.162:9200"
+
+	// Needs to update this bucket based on your gcs bucket name.
+	BUCKET_NAME = "post-image"
+)
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+
+	// range is optional
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
+
+	fmt.Printf("Search received: %f %f %s\n", lat, lon, ran)
+
+	// Create a client (a new connection to es)
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Define geo distance query as specified in
+	// https://www.elastic.co/guide/en/elasticsearch/reference/5.2/query-dsl-geo-distance-query.html
+	q := elastic.NewGeoDistanceQuery("location")
+	q = q.Distance(ran).Lat(lat).Lon(lon)
+
+	// Some delay may range from seconds to minutes. So if you don't get enough results. Try it later.
+	searchResult, err := client.Search().
+		Index(INDEX).
+		Query(q).
+		Pretty(true).
+		Do()
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	// TotalHits is another convenience function that works even when something goes wrong.
+	fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization.
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // instance of
+		p := item.(Post) // p = (Post) item
+		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
+		ps = append(ps, p)
+
+	}
+	js, err := json.Marshal(ps)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(js)
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
+
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	// After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+	// If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+	r.ParseMultipartForm(32 << 20)
+
+	// Parse from form data.
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+	p := &Post{
+		User:    username.(string),
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
+	id := uuid.New()
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusInternalServerError)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
+	}
+	defer file.Close()
+
+	ctx := context.Background()
+
+	// replace it with your real bucket name.
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
+		return
+	}
+
+	// Update the media link after saving to GCS.
+	p.Url = attrs.MediaLink
+
+	// Save to ES.
+	saveToES(p, id)
+
+	// Save to BigTable.
+	//saveToBigTable(p, id)
 }
 
 // Save a post to ElasticSearch
@@ -115,81 +251,46 @@ func saveToES(p *Post, id string) {
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
 }
 
-func handlerSearch(w http.ResponseWriter, r *http.Request) {
-	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
-	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer client.Close()
 
-	// range is optional
-	ran := DISTANCE
-	if val := r.URL.Query().Get("range"); val != "" {
-		ran = val + "km"
+	bucket := client.Bucket(bucketName)
+	// Next check if the bucket exists
+	if _, err = bucket.Attrs(ctx); err != nil {
+		return nil, nil, err
 	}
 
-	fmt.Printf( "Search received: %f %f %s\n", lat, lon, ran)
+	obj := bucket.Object(name)
+	w := obj.NewWriter(ctx)
+	if _, err := io.Copy(w, r); err != nil {
+		return nil, nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, nil, err
+	}
 
-      // Create a client
-      client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
-      if err != nil {
-             panic(err)
-             return
-      }
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
 
-      // Define geo distance query as specified in
-      // https://www.elastic.co/guide/en/elasticsearch/reference/5.2/query-dsl-geo-distance-query.html
-      q := elastic.NewGeoDistanceQuery("location")
-      q = q.Distance(ran).Lat(lat).Lon(lon)
-
-      // Some delay may range from seconds to minutes. So if you don't get enough results. Try it later.
-      searchResult, err := client.Search().
-             Index(INDEX).
-             Query(q).
-             Pretty(true).
-             Do()
-      if err != nil {
-             // Handle error
-             panic(err)
-      }
-
-      // searchResult is of type SearchResult and returns hits, suggestions,
-      // and all kinds of other information from Elasticsearch.
-      fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
-      // TotalHits is another convenience function that works even when something goes wrong.
-      fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
-
-      // Each is a convenience function that iterates over hits in a search result.
-      // It makes sure you don't need to check for nil values in the response.
-      // However, it ignores errors in serialization.
-      var typ Post
-      var ps []Post
-      for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // instance of
-             p := item.(Post) // p = (Post) item
-             fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
-	     if !containsFilteredWords(&p.Message) {
-             	ps = append(ps, p)
-	     }
-
-      }
-      js, err := json.Marshal(ps)
-      if err != nil {
-             panic(err)
-             return
-      }
-
-      w.Header().Set("Content-Type", "application/json")
-      w.Header().Set("Access-Control-Allow-Origin", "*")
-      w.Write(js)
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+	return obj, attrs, err
 }
 
 func containsFilteredWords(s *string) bool {
-        filteredWords := []string{
-                "fuck",
-                "150",
-        }
-        for _, word := range filteredWords {
-                if strings.Contains(*s, word) {
-                        return true
-                }
-        }
-        return false
+	filteredWords := []string{
+		"fuck",
+		"150",
+	}
+	for _, word := range filteredWords {
+		if strings.Contains(*s, word) {
+			return true
+		}
+	}
+	return false
 }
-
